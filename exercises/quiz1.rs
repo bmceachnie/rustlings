1cbe0f7229d9fe2e23dda7dc732e129642718fe1
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2023-01-23 01:08:38 +0100

    personal progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+const APPLE_DISCOUNT_AMOUNT: i32 = 40;
+const APPLE_DISCOUNT_PRICE: i32 = 1;
+const APPLE_PRICE: i32 = 2;
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples > APPLE_DISCOUNT_AMOUNT {
+        return APPLE_DISCOUNT_PRICE * apples;
+    }
+
+    return APPLE_PRICE * apples;
+}
 
 // Don't modify this function!
 #[test]
